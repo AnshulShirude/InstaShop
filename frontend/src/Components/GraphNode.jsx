e1d2bf8d487c58f78f3ff50cbf65dde75edfdf8b
Aryan Kale <aryankale22@gmail.com>
2023-10-21 18:41:54 -0400

    Moving graph to frontend

---
+++ frontend/src/Components/GraphNode.jsx
@@ -0,0 +1,17 @@
+import {Graph, Node, Edge, linkNodes, createBoard} from "../GraphLogic/graph"
+
+export default function GraphNode() {
+
+    const board = createBoard(4, 4);
+    const edges = linkNodes(board);
+    const graph = new Graph(board, edges);
+
+    const node1x = graph.board[0][0].x;
+
+
+    return (
+        <>
+            <p>{node1x}</p>
+        </>
+    );
+}
