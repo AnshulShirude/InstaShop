6a6ef7fd305ac1a9d427cb50cda80bb25ceebd82
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2023-10-22 05:10:24 -0400

    The API works now, the Map tries to recieve value

--- frontend/src/Pages/AddItems.jsx
+++ frontend/src/Pages/AddItems.jsx
@@ -6,95 +6,108 @@ import { NavLink } from 'react-router-dom';
 function AddItems() {
     const [inputValue, setInputValue] = useState('');
     const [storedValues, setStoredValues] = useState([]);
+    const [aisleLoading, setAisleLoading] = useState(false);
+    const [foodAisleMap, setFoodAisleMap] = useState({});
     const key = '0CC42161E0A649A2AC3625E7CE64A56E';
 
-    // Start with an empty Local Dictionary/Map of foods and their aisle numbers
-    const [foodAisleMap, setFoodAisleMap] = useState({});
+    // Predefined local map of 20 food items and their aisle numbers
+    const LOCAL_FOOD_AISLE_MAP = {
+        'beef': 'C9',
+        'chicken': 'C9',
+        'goldfish': 'C10',
+        'fruit snack': 'C10',
+        'ham': 'C8',
+        'cheese': 'C5',
+        'bagel': 'C1',
+        'eyeliner': 'B13',
+        'toilet': 'D26',
+        'detergent': 'D22',
+        'broom': 'D21',
+        'toothpaste': 'B6',
+        'deodarant': 'B5',
+        'football': 'D6',
+        'headphone': 'A6',
+        'charger': 'A5',
+        'shoes': 'D4'
+    };
 
     const handleInputChange = event => {
         setInputValue(event.target.value);
     };
 
     const handleSubmit = () => {
-        const lowerCaseInputValue = inputValue.trim().toLowerCase(); // Convert to lowercase
+      const lowerCaseInputValue = inputValue.trim().toLowerCase(); 
 
-        if (lowerCaseInputValue !== '') {
-            // Check if the item exists in the local dictionary
-            if (foodAisleMap[lowerCaseInputValue]) {
-                setStoredValues(prevValues => [...prevValues, {
-                    inputValue: lowerCaseInputValue,
-                    itemName: lowerCaseInputValue,
-                    aisleNumber: foodAisleMap[lowerCaseInputValue],
-                    loading: false
-                }]);
-                setInputValue('');
-            } else {
-                const newValue = {
-                    inputValue: lowerCaseInputValue,
-                    itemName: '',
-                    aisleNumber: '',
-                    loading: true
-                };
+      // First check if the food item exists in the local predefined map
+      if (LOCAL_FOOD_AISLE_MAP[lowerCaseInputValue]) {
+          setStoredValues(prevValues => [...prevValues, {
+              inputValue: lowerCaseInputValue,
+              itemName: lowerCaseInputValue,
+              aisleNumber: LOCAL_FOOD_AISLE_MAP[lowerCaseInputValue],
+              loading: false
+          }]);
+          setInputValue('');
+      } 
+      // If not in the local map, check in the existing state or fetch from API
+      else {
+        const newValue = {
+            inputValue: lowerCaseInputValue,
+            itemName: lowerCaseInputValue,
+            aisleNumber: '',
+            loading: true
+        };
 
-                setStoredValues(prevValues => [...prevValues, newValue]);
-                setInputValue('');
+        setStoredValues(prevValues => [...prevValues, newValue]);
+        setInputValue('');
 
-                // Fetch data from API if not in local dictionary
-                fetchData(newValue, storedValues.length);
-            }
-        }
-    };
+        fetchData(newValue, storedValues.length);
+    }
+};
 
-    const fetchData = async (value, index) => {
-        try {
-            const response = await axios.get('https://api.redcircleapi.com/request', {
-                params: {
-                    api_key: key,
-                    search_term: value.inputValue, // Use value instead of direct inputValue
-                    type: 'search',
-                    customer_zipcode: '02215',
-                    delivery_type: 'in_store_pickup',
-                    sort_by: 'best_match',
-                    output: 'json'
-                }
-            });
+const fetchData = async (value, index) => {
+  try {
+      setAisleLoading(true);
 
-            const product_tcin = response.data.search_results[0].product.tcin;
-            const product_title = response.data.search_results[0].product.title;
+      const response = await axios.get('https://api.redcircleapi.com/request', {
+          params: {
+              api_key: key,
+              search_term: value.inputValue,
+              type: 'search',
+              customer_zipcode: '02215-1205',
+              delivery_type: 'in_store_pickup',
+              output: 'json'
+          }
+      });
 
-            // Update the specific stored value with the fetched item name immediately
-            setStoredValues(prevValues => {
-                const updatedValues = [...prevValues];
-                updatedValues[index].itemName = product_title;
-                return updatedValues;
-            });
+      const product_tcin = response.data.search_results[0].product.tcin;
 
-            const aisleResponse = await axios.get('https://api.redcircleapi.com/request', {
-                params: {
-                    api_key: key,
-                    type: 'product',
-                    tcin: product_tcin,
-                    customer_zipcode: '02215',
-                    output: 'json'
-                }
-            });
+      const aisleResponse = await axios.get('https://api.redcircleapi.com/request', {
+          params: {
+              api_key: key,
+              type: 'product',
+              tcin: product_tcin,
+              customer_zipcode: '02215-1205',
+              output: 'json'
+          }
+      });
 
-            const aisleNumber = aisleResponse.data.product.aisle;
-            setFoodAisleMap(prevMap => ({ ...prevMap, [value.inputValue]: aisleNumber }));
+      const aisleNumber = aisleResponse.data.product.aisle;
+      setFoodAisleMap(prevMap => ({ ...prevMap, [value.inputValue]: aisleNumber }));
 
-            // Update the specific stored value with the fetched aisle number and set loading to false
-            setStoredValues(prevValues => {
-                const updatedValues = [...prevValues];
-                updatedValues[index].aisleNumber = aisleNumber;
-                updatedValues[index].loading = false;
-                return updatedValues;
-            });
+      setStoredValues(prevValues => {
+          const updatedValues = [...prevValues];
+          updatedValues[index].aisleNumber = aisleNumber;
+          updatedValues[index].loading = false;
+          return updatedValues;
+      });
 
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+      setAisleLoading(false);
 
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+  
+};
     return (
         <div className="AddItems">
             <div>
@@ -104,6 +117,7 @@ function AddItems() {
                     onChange={handleInputChange}
                 />
                 <button onClick={handleSubmit}>Submit</button>
+                {/*  disabled={aisleLoading} */}
             </div>
 
             <h3>Stored Values:</h3>
--- frontend/src/Pages/Map.jsx
+++ frontend/src/Pages/Map.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
 
 function MapPage() {
+    const location = useLocation();
+    const { aisleNumbers } = location.state || {};
     return (
         <div>
-            <h1>Map</h1>
-            <p>The map is shown below:</p>
+          <h1>Map Page</h1>
+          {aisleNumbers ? (
+            <div>
+              <h2>Aisle Numbers:</h2>
+              <ul>
+                {aisleNumbers.map((aisleNumber, index) => (
+                  <li key={index}>{aisleNumber}</li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No aisle numbers found.</p>
+          )}
         </div>
-    );
-}
+      );
+    }
 
 export default MapPage;
