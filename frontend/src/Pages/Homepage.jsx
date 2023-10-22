0c86f10364117ce73cda83c778af276d520d705a
justagoodfriend <greenate1@gmail.com>
2023-10-22 03:47:49 -0400

    some basic cleanup not much styling

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Homepage() {
-    return (
-        <div>
-            <h1>Welcome!</h1>
-            <p>Welcome to the Application!</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to Instacart</h1>
+      <span>
+        To Start Click on the Add Items link in the sidebar, to add items into
+        your shopping cart Once Commplete, we will generate your quicket path
+        throughout the store accessing those items
+      </span>
+    </div>
+  );
 }
 
 export default Homepage;
--- frontend/src/Pages/Map.jsx
+++ frontend/src/Pages/Map.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import MasterGraph from "../Components/MasterGraph";
+import styles from "../App.css";
 
 function MapPage() {
-    return (
-        <div>
-            <h1>Map</h1>
-            <p>The map is shown below:</p>
-        </div>
-    );
+  const graphStyle = {
+    display: "flex",
+    justifyContent: "center",
+  };
+
+  return (
+    <div>
+      <h1>Map</h1>
+      <p>The map is shown below:</p>
+      <div style={graphStyle}>
+        <MasterGraph />
+      </div>
+    </div>
+  );
 }
 
 export default MapPage;
