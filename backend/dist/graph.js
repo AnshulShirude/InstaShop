c3d382602fa9b360ddd24621c2cbcd56990af27f
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2023-10-22 01:08:46 -0400

    Should be working algo

--- backend/dist/graph.js
+++ backend/dist/graph.js
@@ -1,5 +1,9 @@
 class Node {
     constructor(x, y) {
+        this.north = undefined;
+        this.south = undefined;
+        this.east = undefined;
+        this.west = undefined;
         this.withinBorder = false;
         this.visited = false;
         this.x = x;
@@ -11,34 +15,144 @@ class Node {
     resetVisited() {
         this.visited = false;
     }
+    print() {
+        const neighbors = this.getNeighbors();
+        let string = "Postion: (" + this.x + "," + this.y + ")" + " Neighbors :";
+        for (const neighbor of neighbors) {
+            string += "(" + neighbor.x + "," + neighbor.y + ")";
+        }
+        return string;
+    }
+    getNeighbors() {
+        const NodeList = [];
+        if (this.north) {
+            NodeList.push(this.north);
+        }
+        if (this.south) {
+            NodeList.push(this.south);
+        }
+        if (this.west) {
+            NodeList.push(this.west);
+        }
+        if (this.east) {
+            NodeList.push(this.east);
+        }
+        return NodeList;
+    }
+}
+class Edge {
+    constructor(NodeTo, NodeFrom) {
+        this.NodeTo = NodeTo;
+        this.NodeFrom = NodeFrom;
+    }
+    linkCells() {
+        if (this.NodeFrom.x === this.NodeTo.x) {
+            if (this.NodeFrom.y > this.NodeTo.y) {
+                this.NodeTo.south = this.NodeFrom;
+                this.NodeFrom.north = this.NodeTo;
+            }
+            else {
+                this.NodeTo.north = this.NodeFrom;
+                this.NodeFrom.south = this.NodeTo;
+            }
+        }
+        else {
+            if (this.NodeFrom.x > this.NodeTo.x) {
+                this.NodeTo.east = this.NodeFrom;
+                this.NodeFrom.west = this.NodeTo;
+            }
+            else {
+                this.NodeTo.west = this.NodeFrom;
+                this.NodeFrom.east = this.NodeTo;
+            }
+        }
+    }
 }
 class Graph {
-    constructor(rows, cols) {
-        this.board = [];
-        this.rows = rows;
-        this.cols = cols;
-        for (let i = 0; i < rows; i++) {
-            const row = [];
-            for (let j = 0; j < cols; j++) {
-                row.push(new Node(i, j));
-            }
-            this.board.push(row);
-        }
+    constructor(board, edges) {
+        this.board = board;
+        this.edges = edges;
+        this.itemTranslation = new Map();
+        // for (let i = 0; i < this.board.length; i++) {
+        //     const row: Node[] = [];
+        //     for (let j = 0; j < this.board[i].length; j++) {
+        //         row.push(new Node(i, j));
+        //     }
+        //     this.board.push(row);
+        // }
     }
     getNode(x, y) {
         return this.board[x][y];
     }
     getNeighbors(node) {
-        const moves = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+        const moves = [];
+        if (node.west) {
+            moves.push([-1, 0]);
+        }
+        if (node.east) {
+            moves.push([1, 0]);
+        }
+        if (node.north) {
+            moves.push([0, -1]);
+        }
+        if (node.south) {
+            moves.push([0, 1]);
+        }
+        // console.log("These are the moves: ", moves);
+        // const moves = [[-1, 0], [0, 1], [1, 0], [0, -1]];
         const neighbors = [];
         for (const [dx, dy] of moves) {
             const nx = node.x + dx;
             const ny = node.y + dy;
-            if (nx >= 0 && nx < this.rows && ny >= 0 && ny < this.cols) {
+            if (nx >= 0 && nx < this.board.length && ny >= 0 && ny < this.board[0].length) {
                 neighbors.push(this.board[nx][ny]);
             }
         }
         return neighbors;
+    }
+    // adds an item on our Map to represent a Node
+    createTransalation(aisleName, node) {
+        this.itemTranslation.set(aisleName, node);
+    }
+    createAisles(upperLeft, lowerRight) {
+        let leftX = upperLeft.x;
+        let rightX = lowerRight.x;
+        let northY = upperLeft.y;
+        let southY = lowerRight.y;
+        for (let i = leftX; i <= rightX; i++) {
+            for (let j = northY; j <= southY; j++) {
+                const currentNode = this.board[i][j];
+                currentNode.withinBorder = true;
+                if (i === leftX) {
+                    if (i >= 1) {
+                        const prevLeftNode = this.board[i - 1][j];
+                        prevLeftNode.east = null;
+                        currentNode.west = null;
+                    }
+                }
+                if (i === rightX) {
+                    if (i < this.board.length - 2) {
+                        const forwardRightNode = this.board[i + 1][j];
+                        forwardRightNode.west = null;
+                        currentNode.east = null;
+                    }
+                }
+                if (j === southY) {
+                    if (j < this.board[0].length - 2) {
+                        const prevBelowNode = this.board[i][j + 1];
+                        prevBelowNode.north = null;
+                        currentNode.south = null;
+                    }
+                }
+                if (j === northY) {
+                    if (j >= 1) {
+                        const forwardBelowNode = this.board[i][j - 1];
+                        forwardBelowNode.south = null;
+                        currentNode.north = null;
+                    }
+                }
+            }
+        }
     }
     bfs(start, end) {
         const queue = [[start, [start]]];
@@ -93,6 +207,43 @@ class Graph {
         return bestPath;
     }
 }
+function createBoard(width, height) {
+    let board = [];
+    for (let i = 0; i < width; i++) {
+        board[i] = [];
+    }
+    //Populate the Grid with Nodes at each index
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            board[i][j] = new Node(i, j);
+        }
+    }
+    return board;
+}
+function linkNodes(board) {
+    const height = board[0].length;
+    const width = board.length;
+    const edges = [];
+    // this is for the columns
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height - 1; j++) {
+            const currentCell = board[i][j];
+            const nextCell = board[i][j + 1];
+            const edge = new Edge(currentCell, nextCell);
+            edges.push(edge);
+        }
+    }
+    // this is for the rows
+    for (let j = 0; j < height; j++) {
+        for (let i = 0; i < width - 1; i++) {
+            const currentCell = board[i][j];
+            const nextCell = board[i + 1][j];
+            const edge = new Edge(currentCell, nextCell);
+            edges.push(edge);
+        }
+    }
+    return edges;
+}
 // Usage1:
 // const graph = new Graph(10, 10);
 // const startNode = graph.getNode(9, 9);
@@ -104,13 +255,43 @@ class Graph {
 // const endNode = graph.getNode(0, 3);
 // const nodesToCover = [graph.getNode(0,0), graph.getNode(3, 0), graph.getNode(0, 1), graph.getNode(0, 2), graph.getNode(2, 1)];
 //Usage 3
-const graph = new Graph(80, 24);
-const startNode = graph.getNode(79, 23);
-const endNode = graph.getNode(0, 23);
-const nodesToCover = [graph.getNode(0, 0), graph.getNode(3, 0), graph.getNode(0, 1), graph.getNode(0, 2), graph.getNode(2, 1)];
-const path = graph.shortestPathBFS(startNode, endNode, nodesToCover);
+//////////////////////////////////////////////////////// BIG BOARD //////////////////////////////////////////////////////////
+// const board = createBoard(80, 24);
+// const edges = linkNodes(board);
+// for (const edge of edges) {
+//   edge.linkCells();
+// }
+// const graph = new Graph(board, edges);
+// const startNode = graph.getNode(79, 23);
+// const endNode = graph.getNode(0, 23);
+// const nodesToCover = [graph.getNode(0,0), graph.getNode(3, 0), graph.getNode(0, 1), graph.getNode(0, 2), graph.getNode(2, 1)];
+// const path = graph.shortestPathBFS(startNode, endNode, nodesToCover);
+// console.log(`Shortest path length: ${path.length}`);
+// const pathCoords = [];
+// for (let node of path) {
+//   pathCoords.push('X: ' + node.x + " Y: " + node.y )
+// }
+// console.log('Path:', pathCoords);
+//////////////////////////////////////////////////////// BIG BOARD //////////////////////////////////////////////////////////
+const board1 = createBoard(10, 10);
+const edges1 = linkNodes(board1);
+for (const edge1 of edges1) {
+    edge1.linkCells();
+}
+const graph1 = new Graph(board1, edges1);
+graph1.createAisles(board1[2][6], board1[2][8]);
+graph1.createAisles(board1[5][1], board1[5][5]);
+graph1.createAisles(board1[8][6], board1[8][8]);
+const startNode = graph1.getNode(9, 9);
+const endNode = graph1.getNode(0, 9);
+const nodesToCover = [graph1.getNode(0, 7), graph1.getNode(2, 3), graph1.getNode(5, 7), graph1.getNode(7, 8), graph1.getNode(8, 2)];
+const path = graph1.shortestPathBFS(startNode, endNode, nodesToCover);
 console.log(`Shortest path length: ${path.length}`);
-console.log('Path:', path);
+const pathCoords = [];
+for (let node of path) {
+    pathCoords.push([node.x, node.y]);
+}
+console.log('Path:', pathCoords);
 export {};
 // class Node {
 //   constructor(public x: number, public y: number) {}
--- backend/src/graph.ts
+++ backend/src/graph.ts
@@ -1,6 +1,10 @@
 class Node {
   x: number;
   y: number;
+  north = undefined;
+  south = undefined;
+  east = undefined;
+  west = undefined;
   withinBorder = false;
   visited = false;
 
@@ -9,6 +13,7 @@ class Node {
     this.y = y;
   }
 
+
   equals(node: Node) {
     return this.x === node.x && this.y === node.y;
   }
@@ -16,24 +21,86 @@ class Node {
   resetVisited() {
     this.visited = false;
   }
+
+  print() {
+    const neighbors = this.getNeighbors();
+    let string = "Postion: (" + this.x + "," + this.y + ")" + " Neighbors :";
+    for (const neighbor of neighbors) {
+      string += "(" + neighbor.x + "," + neighbor.y + ")";
+    }
+    return string;
+  }
+
+  getNeighbors() {
+    const NodeList: Node[] = [];
+    if (this.north) {
+      NodeList.push(this.north);
+    }
+
+    if (this.south) {
+      NodeList.push(this.south);
+    }
+    if (this.west) {
+      NodeList.push(this.west);
+    }
+
+    if (this.east) {
+      NodeList.push(this.east);
+    }
+
+    return NodeList;
+  }
 }
 
-class Graph {
-  private board: Node[][] = [];
-  public rows: number;
-  public cols: number;
+class Edge {
+  NodeTo: Node;
+  NodeFrom: Node;
 
-  constructor(rows: number, cols: number) {
-      this.rows = rows;
-      this.cols = cols;
+  constructor(NodeTo: Node, NodeFrom: Node) {
+    this.NodeTo = NodeTo;
+    this.NodeFrom = NodeFrom;
+  }
 
-      for (let i = 0; i < rows; i++) {
-          const row: Node[] = [];
-          for (let j = 0; j < cols; j++) {
-              row.push(new Node(i, j));
-          }
-          this.board.push(row);
+  linkCells() {
+    if (this.NodeFrom.x === this.NodeTo.x) {
+      if (this.NodeFrom.y > this.NodeTo.y) {
+        this.NodeTo.south = this.NodeFrom;
+        this.NodeFrom.north = this.NodeTo;
+      } else {
+        this.NodeTo.north = this.NodeFrom;
+        this.NodeFrom.south = this.NodeTo;
       }
+    } else {
+      if (this.NodeFrom.x > this.NodeTo.x) {
+        this.NodeTo.east = this.NodeFrom;
+        this.NodeFrom.west = this.NodeTo;
+      } else {
+        this.NodeTo.west = this.NodeFrom;
+        this.NodeFrom.east = this.NodeTo;
+      }
+    }
+  }
+}
+
+
+class Graph {
+  board: Node[][];
+  edges: Edge[];
+  itemTranslation: Map<string, Node>;
+  pathFound: boolean;
+
+  constructor(board: Node[][], edges: Edge[]) {
+    this.board = board;
+    this.edges = edges;
+    this.itemTranslation = new Map();
+
+      // for (let i = 0; i < this.board.length; i++) {
+      //     const row: Node[] = [];
+      //     for (let j = 0; j < this.board[i].length; j++) {
+      //         row.push(new Node(i, j));
+      //     }
+      //     this.board.push(row);
+      // }
   }
 
   public getNode(x: number, y: number): Node {
@@ -41,19 +108,92 @@ class Graph {
   }
 
   private getNeighbors(node: Node): Node[] {
-      const moves: [number, number][] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+    const moves = []
+    if (node.west) {
+      moves.push([-1, 0]);
+    }
+    if (node.east) {
+      moves.push([1, 0]);
+    }
+    if (node.north) {
+      moves.push([0, -1]);
+    }
+    if (node.south) {
+      moves.push([0, 1]);
+    }
+
+    // console.log("These are the moves: ", moves);
+
+    // const moves = [[-1, 0], [0, 1], [1, 0], [0, -1]];
       const neighbors: Node[] = [];
 
       for (const [dx, dy] of moves) {
           const nx = node.x + dx;
           const ny = node.y + dy;
 
-          if (nx >= 0 && nx < this.rows && ny >= 0 && ny < this.cols) {
+          if (nx >= 0 && nx < this.board.length && ny >= 0 && ny < this.board[0].length) {
               neighbors.push(this.board[nx][ny]);
           }
       }
 
       return neighbors;
+  }
+
+  // adds an item on our Map to represent a Node
+  createTransalation(aisleName: string, node: Node) {
+    this.itemTranslation.set(aisleName, node);
+  }
+
+  createAisles(upperLeft: Node, lowerRight: Node) {
+    let leftX = upperLeft.x;
+    let rightX = lowerRight.x;
+
+    let northY = upperLeft.y;
+    let southY = lowerRight.y;
+
+    for (let i = leftX; i <= rightX; i++) {
+      for (let j = northY; j <= southY; j++) {
+        const currentNode = this.board[i][j];
+        currentNode.withinBorder = true;
+
+        if (i === leftX) {
+          if (i >= 1) {
+            const prevLeftNode = this.board[i - 1][j];
+            prevLeftNode.east = null;
+
+            currentNode.west = null;
+          }
+        }
+
+        if (i === rightX) {
+          if (i < this.board.length - 2) {
+            const forwardRightNode = this.board[i + 1][j];
+            forwardRightNode.west = null;
+
+            currentNode.east = null;
+          }
+        }
+
+        if (j === southY) {
+          if (j < this.board[0].length - 2) {
+            const prevBelowNode = this.board[i][j + 1];
+            prevBelowNode.north = null;
+
+            currentNode.south = null;
+          }
+        }
+
+        if (j === northY) {
+          if (j >= 1) {
+            const forwardBelowNode = this.board[i][j - 1];
+            forwardBelowNode.south = null;
+
+            currentNode.north = null;
+          }
+        }
+      }
+    }
   }
 
   public bfs(start: Node, end: Node): Node[] {
@@ -120,6 +260,52 @@ class Graph {
   }
 }
 
+function createBoard(width: number, height: number) {
+  let board = [];
+  for (let i = 0; i < width; i++) {
+    board[i] = [];
+  }
+
+  //Populate the Grid with Nodes at each index
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      board[i][j] = new Node(i, j);
+    }
+  }
+
+  return board;
+}
+
+function linkNodes(board: Node[][]) {
+  const height = board[0].length;
+  const width = board.length;
+  const edges: Edge[] = [];
+
+  // this is for the columns
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height - 1; j++) {
+      const currentCell = board[i][j];
+      const nextCell = board[i][j + 1];
+
+      const edge = new Edge(currentCell, nextCell);
+      edges.push(edge);
+    }
+  }
+
+  // this is for the rows
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width - 1; i++) {
+      const currentCell = board[i][j];
+      const nextCell = board[i + 1][j];
+
+      const edge = new Edge(currentCell, nextCell);
+      edges.push(edge);
+    }
+  }
+
+  return edges;
+}
+
 
 // Usage1:
 // const graph = new Graph(10, 10);
@@ -136,15 +322,60 @@ class Graph {
 // const endNode = graph.getNode(0, 3);
 // const nodesToCover = [graph.getNode(0,0), graph.getNode(3, 0), graph.getNode(0, 1), graph.getNode(0, 2), graph.getNode(2, 1)];
 //Usage 3
-const graph = new Graph(80, 24);
 
-const startNode = graph.getNode(79, 23);
-const endNode = graph.getNode(0, 23);
-const nodesToCover = [graph.getNode(0,0), graph.getNode(3, 0), graph.getNode(0, 1), graph.getNode(0, 2), graph.getNode(2, 1)];
 
-const path = graph.shortestPathBFS(startNode, endNode, nodesToCover);
-console.log(`Shortest path length: ${path.length}`);
-console.log('Path:', path);
+//////////////////////////////////////////////////////// BIG BOARD //////////////////////////////////////////////////////////
+// const board = createBoard(80, 24);
+// const edges = linkNodes(board);
+// for (const edge of edges) {
+//   edge.linkCells();
+// }
+
+// const graph = new Graph(board, edges);
+
+// const startNode = graph.getNode(79, 23);
+// const endNode = graph.getNode(0, 23);
+// const nodesToCover = [graph.getNode(0,0), graph.getNode(3, 0), graph.getNode(0, 1), graph.getNode(0, 2), graph.getNode(2, 1)];
+
+// const path = graph.shortestPathBFS(startNode, endNode, nodesToCover);
+// console.log(`Shortest path length: ${path.length}`);
+// const pathCoords = [];
+// for (let node of path) {
+//   pathCoords.push('X: ' + node.x + " Y: " + node.y )
+// }
+// console.log('Path:', pathCoords);
+
+//////////////////////////////////////////////////////// BIG BOARD //////////////////////////////////////////////////////////
+
+
+
+
+
+  const board1 = createBoard(10, 10);
+  const edges1 = linkNodes(board1);
+
+  for (const edge1 of edges1) {
+    edge1.linkCells();
+  }
+  const graph1 = new Graph(board1, edges1);
+
+  graph1.createAisles(board1[2][6], board1[2][8]);
+  graph1.createAisles(board1[5][1], board1[5][5]);
+  graph1.createAisles(board1[8][6], board1[8][8]);
+
+  const startNode = graph1.getNode(9,9);
+  const endNode = graph1.getNode(0, 9);
+  const nodesToCover = [graph1.getNode(0,7), graph1.getNode(2, 3), graph1.getNode(5, 7), graph1.getNode(7, 8), graph1.getNode(8, 2)];
+
+  const path = graph1.shortestPathBFS(startNode, endNode, nodesToCover);
+  console.log(`Shortest path length: ${path.length}`);
+  const pathCoords = [];
+  for (let node of path) {
+    pathCoords.push([node.x, node.y])
+  }
+  console.log('Path:', pathCoords);
+
+
 
 
 // class Node {
